98477bec96fe194938f12cc9f7de7cd8ffbcd7ec
McToady.eth <78250146+McCoady@users.noreply.github.com>
2025-02-05 12:41:51 +0900

    nested headers + refactor to library

---
+++ src/lib.rs
@@ -0,0 +1,70 @@
+use std::path::PathBuf;
+use std::fs::{read_dir, read_to_string, File};
+use std::io::Write;
+
+pub fn traverse_dir(
+    path: &PathBuf,
+    file :&mut File,
+    traverse: &bool,
+    level: usize
+) -> u32 {
+    let mut code_count = 0;
+    // First handle all the files at this level
+    for entry in read_dir(path).expect("failed to read path") {
+        let entry = entry.expect("failed to handle");
+        let path = entry.path();
+        if path.is_file() {
+            if let Some(extension) = entry.path().extension() {
+                if extension == "sol" {
+                    // Process the file
+                    code_count += write_output(file, &entry.path());                
+                }
+            }
+        }
+    }
+    // Then traverse through other directories
+    if *traverse {
+        for entry in read_dir(path).expect("failed to read path") {
+            let entry = entry.expect("failed to handle file");
+            let path = entry.path();
+            if path.is_dir() {
+                let dir_name = &path.file_name().unwrap().to_string_lossy();
+                let header = "#".repeat(level);
+                writeln!(file, "{header} {dir_name}").unwrap();
+                code_count += traverse_dir(&path, file, &true, level+1);
+            }
+        }
+    }
+    code_count
+}
+
+fn write_output(file: &mut File, entry: &PathBuf) -> u32 {
+    let content = read_to_string(entry).unwrap();
+    let mut code_count = 0;
+
+    let mut multi_line_comment = false;
+    for line in content.lines() {
+        // Handle being inside a multiline comment
+        if multi_line_comment {
+            if line.contains("*/") {
+                multi_line_comment = false;
+            }
+            continue;
+        } 
+        match line.trim() {
+            val if val.len() == 0 => continue,
+            val if val.starts_with("//") => continue,
+            val if val.starts_with("/*") => {
+                multi_line_comment = true;
+            },
+            _ => code_count += 1,
+        }
+    }
+
+    // Write - `FileName.sol` (COUNT) to file
+    let file_name = &entry.file_name().unwrap().to_string_lossy();
+    let output = format!("- `{}` ({})", &file_name, code_count);
+    writeln!(file, "{}", output).unwrap();
+
+    code_count
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use clap::Parser;
 use std::path::PathBuf;
-use std::fs::{read_dir, read_to_string, File, OpenOptions};
+use std::fs::OpenOptions;
 use std::io::Write;
+
+use tloc::traverse_dir;
 
 #[derive(Parser)]
 #[command(name = "tloc")]
@@ -18,8 +20,6 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    let mut total_code_count = 0;
-
     let mut file = OpenOptions::new()
         .write(true)        // Enable append mode
         .create(true)       // Create the file if it doesn't exist
@@ -28,77 +28,13 @@ fn main() {
     
     writeln!(file, "# Scope").unwrap();
 
-    // Handle user's path is a specific file location
-    // Handle user's path is a directory
-    // Handle user error where user passed neither
-    if args.path.is_file() {
-        if let Some(extension) = args.path.extension() {
-            if extension == "sol" {
-                total_code_count = write_output(&mut file, &args.path);
-            }
-        }       
-    } else if args.path.is_dir() {
-        total_code_count = traverse_dir(&args.path, &mut file, &args.recurse);
-    } else {
-        panic!("PATH READ ERROR");
+    let path = args.path;
+    if !path.is_file() && !path.is_dir() {
+        panic!("Incorrect Path Provided");
     }
+
+    let total_code_count = traverse_dir(&path, &mut file, &args.recurse, 2);
 
     writeln!(file, "TOTAL: {}", total_code_count).unwrap();
-}
-
-pub fn traverse_dir(path: &PathBuf, file :&mut File, traverse: &bool) -> u32 {
-    let mut code_count = 0;
-    // Loop over directory
-    for entry in read_dir(path).expect("failed to read path") {
-        let entry = entry.expect("failed to handle file");
-        let path = entry.path();
-
-        // If file calc SLOC & write to file
-        // If directory traverse directory (IF traverse == true)
-        if path.is_file() {
-            if let Some(extension) = entry.path().extension() {
-                if extension == "sol" {  // For Solidity files
-                    // Process the file
-                    code_count += write_output(file, &entry.path());                
-                }
-            }
-        } else if path.is_dir() {
-            if *traverse {
-                code_count += traverse_dir(&path, file, &true);
-            }
-        }
-    }
-    code_count
-}
-
-pub fn write_output(file: &mut File, entry: &PathBuf) -> u32 {
-    let content = read_to_string(entry).unwrap();
-    let mut code_count = 0;
-
-    let mut multi_line_comment = false;
-    for line in content.lines() {
-        // Handle being inside a multiline comment
-        if multi_line_comment {
-            if line.contains("*/") {
-                multi_line_comment = false;
-            }
-            continue;
-        } 
-        match line.trim() {
-            val if val.len() == 0 => continue,
-            val if val.starts_with("//") => continue,
-            val if val.starts_with("/*") => {
-                multi_line_comment = true;
-            },
-            _ => code_count += 1,
-        }
-    }
-
-    // Write - `FileName.sol` (COUNT) to file
-    let file_name = &entry.file_name().unwrap().to_string_lossy();
-    let output = format!("- `{}` ({})", &file_name, code_count);
-    writeln!(file, "{}", output).unwrap();
-
-    code_count
 }
 
